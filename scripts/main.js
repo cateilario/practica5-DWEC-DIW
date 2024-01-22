107da9327bdaca2079567138dec078402eed9dec
Cate <caterinailariopaz@alumnos.tuniversformacion.es>
2024-01-22 11:01:01 +0100

    añadir card footer

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,34 @@ const toggleMenu = () => {
 
 openBtn.addEventListener('click', toggleMenu);
 closeBtn.addEventListener('click', toggleMenu);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cardContainers = document.querySelectorAll('.card-footer');
+
+    // Número máximo de estrellas
+    const maxStars = 5;
+
+    // Iterar sobre cada contenedor de tarjetas y generar las estrellas
+    cardContainers.forEach(function (cardContainer) {
+        // Valoración aleatoria de ejemplo
+        const userRating = Math.floor(Math.random() * maxStars) + 1;
+
+        generateRatingStars(cardContainer, maxStars, userRating);
+    });
+});
+
+function generateRatingStars(container, maxStars, userRating) {
+    // Generar estrellas
+    for (let i = 1; i <= maxStars; i++) {
+        const star = document.createElement('span');
+        star.innerHTML = '&#9733;'; // Estrella sólida
+        star.className = 'star';
+
+        // Valoración aleatoria
+        if (i <= userRating) {
+            star.classList.add('rated');
+        }
+
+        container.appendChild(star);
+    }
+}
